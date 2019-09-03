600f62094829af14cc23918a88bceee2da316f55
anik112 <paulanik112@gmail.com>
2019-09-03 18:05:38 +0600

    update 03/09/2019

---
+++ daleteAttendenceData/src/deleteAttendenceData/MainClass.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deleteAttendenceData;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author VSIANIK
+ */
+public class MainClass extends Application{
+    
+    public static void main(String[] args) throws Throwable {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+       new deleteDataView.DeleteAttendenceOfLeaveEmp().setVisible(true);
+    }
+    
+    
+    
+}
