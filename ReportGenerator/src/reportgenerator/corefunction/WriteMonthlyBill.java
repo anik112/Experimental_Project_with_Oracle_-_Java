402346676cedf749649f4a328ec07ef965df6ed1
anik112 <paulanik112@gmail.com>
2020-05-19 00:38:50 +0600

    Update core code and main function

--- ReportGenerator/src/reportgenerator/corefunction/WriteMonthlyBill.java
+++ ReportGenerator/src/reportgenerator/corefunction/WriteMonthlyBill.java
@@ -5,16 +5,12 @@
  */
 package reportgenerator.corefunction;
 
-import com.itextpdf.text.BaseColor;
-import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.Element;
 import com.itextpdf.text.Font;
 import com.itextpdf.text.FontFactory;
 import com.itextpdf.text.Image;
 import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.Rectangle;
-import com.itextpdf.text.pdf.PRAcroForm;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
@@ -26,7 +22,7 @@ import java.util.logging.Logger;
  *
  * @author Anik
  */
-public class WriteOnFile {
+public class WriteMonthlyBill {
 
     private String date;
     private String billNo;
@@ -47,14 +43,14 @@ public class WriteOnFile {
     private final String tableFooterTxt = "VAT and TAX not included.";
     private final String amountTage = "Amount in word: ";
     private final String noteTxt = "N.B: All payments should be in a/c payee cheque in favour of “ Vistasoft IT Bangladesh Ltd.”";
-    private final String sign = "..................\nAuthorized Signatory";
+    private final String sign = "..................................\nAuthorized Signatory";
 
     public static void main(String[] arg) {
 
-        new WriteOnFile().writeTextInPdfFile();
+        new WriteMonthlyBill().writeMonthlyBillInPdfFile();
     }
 
-    public void writeTextInPdfFile() {
+    public void writeMonthlyBillInPdfFile() {
 
         date = "May 05, 2020";
         billNo = "052020";
@@ -62,7 +58,7 @@ public class WriteOnFile {
         year = "2020";
         companyName = "Alif Embroidery Village Ltd.";
         address = "Bangabandhu Road, Tongabari , Ashulia, Savar, Dhaka.";
-        amount=4000;
+        amount = 4000;
 
         try {
             Document document = new Document();
@@ -71,37 +67,36 @@ public class WriteOnFile {
             document.setMargins(40, 40, 20, 20);
             document.open();
 
-            Image topLogo = Image.getInstance("D:\\\\logoVSI.png");
+            Image topLogo = Image.getInstance("img\\logoWithFooterTxt.jpg");
             topLogo.setAlignment(topLogo.ALIGN_RIGHT);
-            topLogo.scaleAbsolute(150, 50);
+            topLogo.scaleAbsolute(180, 60);
             //topLogo.setSpacingBefore(100);
             document.add(topLogo);
             
-            
-            Font f1 = FontFactory.getFont("SansSerif", 12);
-            
-            Paragraph docHeader = new Paragraph("\n\nDate: " + date+ "\n\n"
+            FontFactory.register("font\\calibri.ttf", "calibri");
+            Font f1 = FontFactory.getFont("calibri", 12);
+
+            Paragraph docHeader = new Paragraph("\nDate: " + date + "\n\n"
                     + "Bill no: " + billNo + "\n\n"
                     + to + "\n"
                     + companyName + "\n"
-                    + address + "\n\n\n"
+                    + address + "\n\n"
                     + greetingtextHead + "\n"
                     + greetingtextBody,
                     f1);
             document.add(docHeader);
-            
-            
+
             PdfPTable table = new PdfPTable(3);
             table.setWidthPercentage(100);
             table.setSpacingBefore(15f);
             table.setSpacingAfter(3f);
-            
-            Font f2=FontFactory.getFont("SansSerif", 12);
 
-            float[] colomWidth = {0.3f, 2f,0.6f};
+            Font f2 = FontFactory.getFont("calibri", 12);
+
+            float[] colomWidth = {0.3f, 2f, 0.6f};
             table.setWidths(colomWidth);
 
-            PdfPCell cellHeader01 = new PdfPCell(new Paragraph(colHeder01,f2));
+            PdfPCell cellHeader01 = new PdfPCell(new Paragraph(colHeder01, f2));
             cellHeader01.setPaddingLeft(10);
             cellHeader01.setPaddingTop(20);
             cellHeader01.setPaddingBottom(20);
@@ -109,51 +104,51 @@ public class WriteOnFile {
             cellHeader01.setHorizontalAlignment(Element.ALIGN_CENTER);
             cellHeader01.setVerticalAlignment(Element.ALIGN_MIDDLE);
 
-            PdfPCell cellHeader02 = new PdfPCell(new Paragraph(new Paragraph(colHeder02,f2)));
+            PdfPCell cellHeader02 = new PdfPCell(new Paragraph(new Paragraph(colHeder02, f2)));
             cellHeader02.setPaddingLeft(10);
             cellHeader02.setPaddingTop(20);
             cellHeader02.setPaddingBottom(20);
             cellHeader02.setPaddingRight(10);
             cellHeader02.setHorizontalAlignment(Element.ALIGN_CENTER);
             cellHeader02.setVerticalAlignment(Element.ALIGN_MIDDLE);
-            
-            PdfPCell cellHeader03 = new PdfPCell(new Paragraph(colHeader03,f2));
+
+            PdfPCell cellHeader03 = new PdfPCell(new Paragraph(colHeader03, f2));
             cellHeader03.setPaddingLeft(10);
             cellHeader03.setPaddingTop(20);
             cellHeader03.setPaddingBottom(20);
             cellHeader03.setPaddingRight(10);
             cellHeader03.setHorizontalAlignment(Element.ALIGN_CENTER);
             cellHeader03.setVerticalAlignment(Element.ALIGN_MIDDLE);
-            
-            float rowBttomPadding=120;
-            float rowTopPadding=20;
-            float padding=10;
-            
-            PdfPCell row01Head = new PdfPCell(new Paragraph("01",f2));
+
+            float rowBttomPadding = 120;
+            float rowTopPadding = 20;
+            float padding = 10;
+
+            PdfPCell row01Head = new PdfPCell(new Paragraph("01", f2));
             row01Head.setPaddingLeft(padding);
             row01Head.setPaddingTop(rowTopPadding);
             row01Head.setPaddingBottom(rowBttomPadding);
             row01Head.setPaddingRight(padding);
             row01Head.setHorizontalAlignment(Element.ALIGN_LEFT);
             row01Head.setVerticalAlignment(Element.ALIGN_TOP);
-            
-            PdfPCell row01Body = new PdfPCell(new Paragraph(rowBody+" "+month+", "+year,f2));
+
+            PdfPCell row01Body = new PdfPCell(new Paragraph(rowBody + " " + month + ", " + year, f2));
             row01Body.setPaddingLeft(padding);
             row01Body.setPaddingTop(rowTopPadding);
             row01Body.setPaddingBottom(rowBttomPadding);
             row01Body.setPaddingRight(padding);
             row01Body.setHorizontalAlignment(Element.ALIGN_LEFT);
             row01Body.setVerticalAlignment(Element.ALIGN_TOP);
-            
-            PdfPCell row01Footer = new PdfPCell(new Paragraph(String.valueOf(amount)+".00",f2));
+
+            PdfPCell row01Footer = new PdfPCell(new Paragraph(String.valueOf(amount) + ".00", f2));
             row01Footer.setPaddingLeft(padding);
             row01Footer.setPaddingTop(rowTopPadding);
             row01Footer.setPaddingBottom(rowBttomPadding);
             row01Footer.setPaddingRight(padding);
             row01Footer.setHorizontalAlignment(Element.ALIGN_CENTER);
             row01Footer.setVerticalAlignment(Element.ALIGN_TOP);
-            
-            PdfPCell row02Body = new PdfPCell(new Paragraph(colFooterTxt,f2));
+
+            PdfPCell row02Body = new PdfPCell(new Paragraph(colFooterTxt, f2));
             row02Body.setPaddingLeft(padding);
             row02Body.setPaddingTop(padding);
             row02Body.setPaddingBottom(padding);
@@ -161,36 +156,68 @@ public class WriteOnFile {
             row02Body.setHorizontalAlignment(Element.ALIGN_RIGHT);
             row02Body.setVerticalAlignment(Element.ALIGN_TOP);
             row02Body.setColspan(2);
-            
-            
-            PdfPCell row02Footer = new PdfPCell(new Paragraph(String.valueOf(amount)+".00",f2));
+
+            PdfPCell row02Footer = new PdfPCell(new Paragraph(String.valueOf(amount) + ".00", f2));
             row02Footer.setPaddingLeft(padding);
             row02Footer.setPaddingTop(padding);
             row02Footer.setPaddingBottom(padding);
             row02Footer.setPaddingRight(padding);
             row02Footer.setHorizontalAlignment(Element.ALIGN_CENTER);
             row02Footer.setVerticalAlignment(Element.ALIGN_TOP);
-            
-                        
+
             table.addCell(cellHeader01);
             table.addCell(cellHeader02);
             table.addCell(cellHeader03);
             table.addCell(row01Head);
-            table.addCell(row01Body);
+            table.addCell(row01Body); 
             table.addCell(row01Footer);
             table.addCell(row02Body);
             table.addCell(row02Footer);
-            
+
             document.add(table);
             
-            Font f3=FontFactory.getFont("SansSerif", 10);
-            document.add(new Paragraph(tableFooterTxt,f3));
+            
+            Font f3 = FontFactory.getFont("SansSerif", 10);
+            document.add(new Paragraph(tableFooterTxt, f3));
 
+            document.add(new Paragraph("\n"+amountTage,f1));
+            document.add(new Paragraph(noteTxt+"\n\n\n",f1));
+            
+            Image sirSing=Image.getInstance("img\\Sir-Sign.jpg");
+            sirSing.setAlignment(sirSing.ALIGN_LEFT);
+            sirSing.scaleAbsolute(100, 30);
+            document.add(sirSing);
+            
+            document.add(new Paragraph(sign+"\n\n",f1));            
+            
+            
+            
+            Image footerImage=Image.getInstance("img\\footer.jpg");
+            footerImage.setAlignment(footerImage.ALIGN_LEFT);
+            footerImage.scaleAbsolute(document.getPageSize().getWidth()-80, 80);
+            document.add(footerImage);
+            
             document.close();
 
         } catch (Exception ex) {
-            Logger.getLogger(WriteOnFile.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(WriteMonthlyBill.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
+//    
+//    private Image getWatermarkedImage(PdfDocument pdfDoc, Image img, String watermark) {
+//        float width = img.getImageScaledWidth();
+//        float height = img.getImageScaledHeight();
+//        float coordX = width / 2;
+//        float coordY = height / 2;
+//        float angle = (float) Math.PI * 30f / 180f;
+//        PdfFormXObject template = new PdfFormXObject(new Rectangle(width, height));
+//        new Canvas(template, pdfDoc)
+//                .add(img)
+//                .setFontColor(DeviceGray.WHITE)
+//                .showTextAligned(watermark, coordX, coordY, TextAlignment.CENTER, angle)
+//                .close();
+//        return new Image(template);
+//    }
 
 }
