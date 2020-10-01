03df9866474b666a8898be1b04625cc4b85313dd
anik112 <paulanik112@gmail.com>
2020-10-01 18:11:57 +0600

    Add new code

--- AutomaticMailSendingSystem/src/main/java/com/vsi/mail/MailSend.java
+++ AutomaticMailSendingSystem/src/main/java/com/vsi/mail/MailSend.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -82,6 +83,8 @@ public class MailSend {
                     return new PasswordAuthentication(username, password);
                 }
             });
+            
+            
 
             String folderPath = "D:\\MailSendingFile\\"; // write file folder path
             File connectFolder = new File(folderPath); // connect with folder location
